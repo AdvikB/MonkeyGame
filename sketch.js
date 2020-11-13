cf6e529b243b6f9175ada8fa4932b8e23920c1d8
AdvikB <73247534+AdvikB@users.noreply.github.com>
2020-11-13 17:52:51 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+
+var monkey, monkey_running, ground
+var banana ,bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var score
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstaceImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  
+monkey = createSprite(80,315,20,20);
+monkey.addAnimation("moving",monkey_running);
+monkey.scale = 0.1;
+
+ground = createSprite(400,350,900,10);  
+ground.velocityX = -4;
+ground.x = ground.width/2;
+console.log(ground.x);
+  
+}
+
+
+function draw() {
+background("255");
+  
+  if (ground.x<0){
+    ground.x = ground.width/2;
+  }
+  
+  if (keyDown("space")){
+    monkey.velocityY = -12;
+  }
+  
+  monkey.velocityY = monkey.velocityY + 0.8;
+  monkey.collide(ground);
+  
+ drawSprites();
+}
+
+
+function Food() {
+   if (frameCount % 80 === 0){
+   banana = createSprite(400,165,10,40);
+   banana.addImage(bananaImage);
+   banana.y = Math.round(random(120,200));
+   banana.velocityX = -5; 
+   banana.lifetime = 270;
+  }
+}   
+ 
+   
+
